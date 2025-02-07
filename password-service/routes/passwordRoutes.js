203c85e4fb0248d88ac913875e4fccd2241d4e42
ashcode12 <ashfaaqbaurtaully@hotmail.com>
2025-02-07 18:41:03 +0100

    Added generate-password endpoint

--- password-service/routes/passwordRoutes.js
+++ password-service/routes/passwordRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { encryptPassword, decryptPassword } = require('../services/passwordService');
+const generatePassword = require('../utils/passwordGenerator');
 
 const router = express.Router();
 
@@ -15,6 +16,30 @@ router.post('/decrypt', (req, res) => {
     const { encryptedPassword } = req.body;
     const password = decryptPassword(encryptedPassword);
     res.json({ password });
+});
+
+// Generate a secure password
+router.post('/generate-password', (req, res) => {
+    const {
+        length,
+        includeLowercase,
+        includeUppercase,
+        includeNumbers,
+        includeSpecialChars,
+    } = req.body;
+
+    try {
+        const password = generatePassword({
+            length,
+            includeLowercase,
+            includeUppercase,
+            includeNumbers,
+            includeSpecialChars,
+        });
+        res.json({ password });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 module.exports = router;
